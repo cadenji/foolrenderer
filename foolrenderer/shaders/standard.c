cc3064d7053646c6464a75f9d65787c60a6a8c57
cadenji <cadenji@qq.com>
2022-05-19 00:31:02 +0800

    Implement a PBR material shader
    Use Filament's PBR material implementation because the
    Filament project includes an excellent documentation. This
    is very helpful for both learning and development.

---
+++ foolrenderer/shaders/standard.c
@@ -0,0 +1,163 @@
+// Copyright (c) Caden Ji. All rights reserved.
+//
+// Licensed under the MIT License. See LICENSE.txt in the project root for
+// license information.
+
+#include "shaders/standard.h"
+
+#include <math.h>
+
+#include "math/math_utility.h"
+#include "math/matrix.h"
+#include "math/vector.h"
+#include "shader_context.h"
+#include "texture.h"
+
+#define TEXCOORD 0
+#define WORLD_SPACE_POSITION 0
+#define WORLD_SPACE_NORMAL 1
+#define WORLD_SPACE_TANGENT 2
+#define WORLD_SPACE_BITANGENT 3
+
+static inline float perceptual_roughness_to_a2(float perceptual_roughness) {
+    // Prevent being zero, and prevent perceptual_oughness^4 from going out of
+    // range of precision.
+    perceptual_roughness = float_max(perceptual_roughness, 0.045f);
+    float roughness = perceptual_roughness * perceptual_roughness;
+    return roughness * roughness;
+}
+
+static inline matrix3x3 construct_tangent2world(struct shader_context *input) {
+    vector3 t =
+        vector3_normalize(*shader_context_vector3(input, WORLD_SPACE_TANGENT));
+    vector3 b = vector3_normalize(
+        *shader_context_vector3(input, WORLD_SPACE_BITANGENT));
+    vector3 n =
+        vector3_normalize(*shader_context_vector3(input, WORLD_SPACE_NORMAL));
+    return matrix3x3_construct(t, b, n);
+}
+
+static inline float pow5(float x) {
+    float x2 = x * x;
+    return x2 * x2 * x;
+}
+
+static inline vector3 f_schlick(vector3 f0, float l_dot_h) {
+    // Schlick's approximation is defined as:
+    // f_schlick = f0 + (1 - f0) * (1 - l_dot_h)^5
+    // This is the optimized code after reducing vector operations.
+    float f = pow5(1.0f - l_dot_h);
+    return vector3_add_scalar(vector3_multiply_scalar(f0, 1.0f - f), f);
+}
+
+static inline float d_ggx(float a2, float n_dot_h) {
+    float f = (n_dot_h * a2 - n_dot_h) * n_dot_h + 1.0;
+    return a2 / (PI * f * f);
+}
+
+static inline float v_smith_ggx_correlated(float a2, float n_dot_l,
+                                           float n_dot_v) {
+    // Height correlated Smith-GGX formulation:
+    // lambda_v = 0.5 * (-1 + sqrt(a2 + (1 - a2) * n_dot_l^2) / n_dot_l)
+    // lambda_l = 0.5 * (-1 + sqrt(a2 + (1 - a2) * n_dot_v^2) / n_dot_v)
+    // g_smith_ggx_correlated = 1 / (1 + lambda_v + lambda_l)
+    // v_smith_ggx_correlated = g_smith_ggx_correlated /
+    //                      (4.0f * n_dot_l * n_dot_v)
+    // This is the optimized code.
+    float lambda_v = n_dot_l * sqrtf((n_dot_v - a2 * n_dot_v) * n_dot_v + a2);
+    float lambda_l = n_dot_v * sqrtf((n_dot_l - a2 * n_dot_l) * n_dot_l + a2);
+    return 0.5 / (lambda_v + lambda_l);
+}
+
+static inline vector3 specular_lobe(float a2, vector3 f0, float n_dot_h,
+                                    float n_dot_l, float n_dot_v,
+                                    float l_dot_h) {
+    // Using Cook-Torrance microfacet BRDF.
+    vector3 f = f_schlick(f0, l_dot_h);
+    float d = d_ggx(a2, n_dot_h);
+    float v = v_smith_ggx_correlated(a2, n_dot_l, n_dot_v);
+    return vector3_multiply_scalar(f, d * v);
+}
+
+static inline vector3 diffuse_lobe(vector3 diffuse_color) {
+    // Using Lambertian BRDF.
+    return vector3_multiply_scalar(diffuse_color, 1.0f / PI);
+}
+
+vector4 standard_vertex_shader(struct shader_context *output,
+                               const void *uniform,
+                               const void *vertex_attribute) {
+    const struct standard_uniform *unif = uniform;
+    const struct standard_vertex_attribute *attr = vertex_attribute;
+
+    vector2 *out_texcoord = shader_context_vector2(output, TEXCOORD);
+    *out_texcoord = attr->texcoord;
+
+    vector4 world_position = matrix4x4_multiply_vector4(
+        unif->local2world, vector3_to_4(attr->position, 1.0f));
+    vector3 *out_position =
+        shader_context_vector3(output, WORLD_SPACE_POSITION);
+    *out_position = vector4_to_3(world_position);
+
+    vector3 *out_normal = shader_context_vector3(output, WORLD_SPACE_NORMAL);
+    *out_normal =
+        matrix3x3_multiply_vector3(unif->local2world_normal, attr->normal);
+
+    vector3 *out_tangent = shader_context_vector3(output, WORLD_SPACE_TANGENT);
+    *out_tangent = matrix3x3_multiply_vector3(unif->loacl2world_direction,
+                                              vector4_to_3(attr->tangent));
+
+    vector3 *out_bitangent =
+        shader_context_vector3(output, WORLD_SPACE_BITANGENT);
+    *out_bitangent = vector3_multiply_scalar(
+        vector3_cross(*out_normal, *out_tangent), attr->tangent.w);
+
+    return matrix4x4_multiply_vector4(unif->world2clip, world_position);
+}
+
+vector4 standard_fragment_shader(struct shader_context *input,
+                                 const void *uniform) {
+    const struct standard_uniform *unif = uniform;
+
+    vector2 texcoord = *shader_context_vector2(input, TEXCOORD);
+    vector3 position = *shader_context_vector3(input, WORLD_SPACE_POSITION);
+
+    vector3 diffuse_color =
+        vector3_multiply_scalar(unif->base_color, (1.0f - unif->metallic));
+
+    float dielectric_f0 =
+        0.16f * unif->reflectance * unif->reflectance * (1.0f - unif->metallic);
+    vector3 conductor_f0 =
+        vector3_multiply_scalar(unif->base_color, unif->metallic);
+    vector3 f0 = vector3_add_scalar(conductor_f0, dielectric_f0);
+
+    float a2 = perceptual_roughness_to_a2(unif->roughness);
+
+    matrix3x3 tangent2world = construct_tangent2world(input);
+    vector3 normal = vector4_to_3(texture_sample(unif->normal_map, texcoord));
+    normal =
+        vector3_subtract_scalar(vector3_multiply_scalar(normal, 2.0f), 1.0f);
+    // Normalized normal, in world space.
+    normal = matrix3x3_multiply_vector3(tangent2world, normal);
+    // Normalized vector from the fragment to the camera, in world space.
+    vector3 view =
+        vector3_normalize(vector3_subtract(unif->camera_position, position));
+    // Normalized halfway vector between the light direction and the view
+    // direction, in world space.
+    vector3 halfway =
+        vector3_normalize(vector3_add(view, unif->light_direction));
+
+    float n_dot_v =
+        float_max(vector3_dot(normal, view), 1e-4f);  // Avoid artifact.
+    float n_dot_l = float_max(vector3_dot(normal, unif->light_direction), 0.0f);
+    float n_dot_h = float_max(vector3_dot(normal, halfway), 0.0f);
+    float l_dot_h =
+        float_max(vector3_dot(unif->light_direction, halfway), 0.0f);
+
+    vector3 fr = specular_lobe(a2, f0, n_dot_h, n_dot_l, n_dot_v, l_dot_h);
+    vector3 fd = diffuse_lobe(diffuse_color);
+    vector3 output =
+        vector3_multiply(vector3_add(fr, fd), unif->light_intensity);
+    output = vector3_multiply_scalar(output, n_dot_l);
+    return vector3_to_4(output, 1.0f);
+}
---
+++ foolrenderer/shaders/standard.h
@@ -0,0 +1,63 @@
+// Copyright (c) Caden Ji. All rights reserved.
+//
+// Licensed under the MIT License. See LICENSE.txt in the project root for
+// license information.
+
+#ifndef FOOLRENDERER_SHADERS_STANDARD_H_
+#define FOOLRENDERER_SHADERS_STANDARD_H_
+
+#include "math/matrix.h"
+#include "math/vector.h"
+#include "shader_context.h"
+#include "texture.h"
+
+// The standard shader implements a physically based rendering (PBR) material
+// model. And use the specific implementation of the Google Filament project:
+// https://google.github.io/filament/Filament.html
+//
+// This model is composed of a diffuse term and a specular term. Can be used to
+// render common opaque metallic/non-metallic objects.
+
+struct standard_uniform {
+    matrix4x4 local2world;
+    matrix4x4 world2clip;
+    matrix3x3 loacl2world_direction;
+    matrix3x3 local2world_normal;
+    // Camera position in world space.
+    vector3 camera_position;
+    // Normalized light direction in world space.
+    vector3 light_direction;
+    vector3 light_intensity;
+
+    struct texture *normal_map;
+    // Diffuse albedo for non-metallic surfaces and specular color for metallic
+    // surfaces, should be in linear color space. A specular color reference
+    // table for metals can be found in the Filament documentation:
+    // https://google.github.io/filament/Filament.html#table_fnormalmetals
+    vector3 base_color;
+    // Whether a surface appears to be dielectric (0.0) or conductor (1.0).
+    float metallic;
+    // Perceived smoothness (0.0) or roughness (1.0) of a surface.
+    float roughness;
+    // Fresnel reflectance at normal incidence for dielectric surfaces, not
+    // useful for conductor surfaces. A reference table of reflectance for
+    // dielectric can be found in the Filament documentation:
+    // https://google.github.io/filament/Filament.html#table_commonmatreflectance
+    float reflectance;
+};
+
+struct standard_vertex_attribute {
+    vector3 position;
+    vector3 normal;
+    vector4 tangent;
+    vector2 texcoord;
+};
+
+vector4 standard_vertex_shader(struct shader_context *output,
+                               const void *uniform,
+                               const void *vertex_attribute);
+
+vector4 standard_fragment_shader(struct shader_context *input,
+                                 const void *uniform);
+
+#endif  // FOOLRENDERER_SHADERS_STANDARD_H_
