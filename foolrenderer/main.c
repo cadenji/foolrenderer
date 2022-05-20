1c96a12f21e97db988cd65de79f4b9a945d25e40
cadenji <cadenji@qq.com>
2022-05-20 14:55:16 +0800

    Little change of file comments

--- foolrenderer/main.c
+++ foolrenderer/main.c
@@ -1,6 +1,6 @@
 // Copyright (c) Caden Ji. All rights reserved.
 //
-// Licensed under the MIT License. See LICENSE.txt in the project root for
+// Licensed under the MIT License. See LICENSE file in the project root for
 // license information.
 
 #include <stdbool.h>
