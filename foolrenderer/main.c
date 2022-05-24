3883dca49ffff4e2ca74e197f508478ac68fce5d
cadenji <cadenji@qq.com>
2022-05-24 17:56:47 +0800

    Rendering rusted metal suzanne model
    Verify the rendering result after using the texture map in the
    standard shader. Rewrite texture loading function to support
    single channel textures.

--- foolrenderer/main.c
+++ foolrenderer/main.c
@@ -18,62 +18,88 @@
 #include "shaders/standard.h"
 #include "texture.h"
 
-#define IMAGE_WIDTH 512
-#define IMAGE_HEIGHT 512
+#define IMAGE_WIDTH 1024
+#define IMAGE_HEIGHT 1024
 
-const vector3 LIGHT_DIRECTION = {{1.0f, 1.0f, 0.5f}};
+const vector3 LIGHT_DIRECTION = {{0.0f, 0.0f, 0.5f}};
 const vector3 CAMERA_POSITION = {{0.0f, 0.0f, 2.5f}};
 
-static void endian_inversion(uint8_t *bytes, size_t size) {
-    uint8_t buffer;
-    size_t left = 0, right = size - 1;
-    while (left < right) {
-        buffer = bytes[left];
-        bytes[left] = bytes[right];
-        bytes[right] = buffer;
-        ++left;
-        --right;
+// Convert TGA image pixels to texture's pixels or texture's pixels to TGA image
+// pixels. Each component of the pixel must be an 8-bit unsigned integer type,
+// and the number of components of pixels must be greater than or equal to 3.
+static inline void pixel_endian_inversion(uint8_t *pixel) {
+    // Swap the values of the 1st and 3rd components of a pixel.
+    uint8_t buffer = pixel[0];
+    pixel[0] = pixel[2];
+    pixel[2] = buffer;
+}
+
+// Modify the components of the TGA image pixels to the order expected by the
+// texture. Only TGA images with pixel formats TGA_PIXEL_RGB24 and
+// TGA_PIXEL_ARGB32 are supported.
+static void modify_tga_image_pixel(uint8_t *image_data, tga_info *image_info) {
+    enum tga_pixel_format pixel_format = tga_get_pixel_format(image_info);
+    if (pixel_format != TGA_PIXEL_RGB24 && pixel_format != TGA_PIXEL_ARGB32) {
+        return;
+    }
+    uint32_t width = tga_get_image_width(image_info);
+    uint32_t height = tga_get_image_height(image_info);
+    for (uint32_t y = 0; y < width; y++) {
+        for (uint32_t x = 0; x < height; x++) {
+            uint8_t *pixel = tga_get_pixel(image_data, image_info, x, y);
+            pixel_endian_inversion(pixel);
+        }
     }
 }
 
-// static struct texture *load_texture(const char *filename) {
-//     if (filename == NULL || strlen(filename) == 0) {
-//         return NULL;
-//     }
+static struct texture *load_texture(const char *filename,
+                                    bool is_srgb_encoding) {
+    if (filename == NULL || strlen(filename) == 0) {
+        return NULL;
+    }
 
-//     uint8_t *image_data;
-//     tga_info *image_info;
-//     enum tga_error error_code;
-//     error_code = tga_load(&image_data, &image_info, filename);
-//     if (error_code != TGA_NO_ERROR) {
-//         return NULL;
-//     }
-//     tga_image_flip_v(image_data, image_info);
+    uint8_t *image_data;
+    tga_info *image_info;
+    enum tga_error error_code;
+    error_code = tga_load(&image_data, &image_info, filename);
+    if (error_code != TGA_NO_ERROR) {
+        return NULL;
+    }
+    enum tga_pixel_format image_pixel_format = tga_get_pixel_format(image_info);
+    uint32_t width = tga_get_image_width(image_info);
+    uint32_t height = tga_get_image_height(image_info);
+    tga_image_flip_v(image_data, image_info);
 
-//     struct texture *texture = NULL;
-//     if (tga_get_pixel_format(image_info) == TGA_PIXEL_RGB24) {
-//         uint32_t width = tga_get_image_width(image_info);
-//         uint32_t height = tga_get_image_height(image_info);
-//         // Convert all pixels to big endian.
-//         uint8_t *pixel;
-//         for (uint32_t y = 0; y < height; y++) {
-//             for (uint32_t x = 0; x < width; x++) {
-//                 pixel = tga_get_pixel(image_data, image_info, x, y);
-//                 endian_inversion(pixel, 3);
-//             }
-//         }
-//         texture = create_texture(TEXTURE_FORMAT_RGB8, width, height);
-//         if (texture != NULL) {
-//             set_texture_pixels(texture, image_data);
-//         }
-//     }
+    struct texture *texture = NULL;
+    if (image_pixel_format == TGA_PIXEL_BW8) {
+        texture = create_texture(TEXTURE_FORMAT_R8, width, height);
+        if (texture != NULL) {
+            set_texture_pixels(texture, image_data);
+        }
+    } else if (image_pixel_format == TGA_PIXEL_RGB24) {
+        modify_tga_image_pixel(image_data, image_info);
+        enum texture_format texture_format =
+            is_srgb_encoding ? TEXTURE_FORMAT_SRGB8 : TEXTURE_FORMAT_RGB8;
+        texture = create_texture(texture_format, width, height);
+        if (texture != NULL) {
+            set_texture_pixels(texture, image_data);
+        }
+    } else if (image_pixel_format == TGA_PIXEL_ARGB32) {
+        modify_tga_image_pixel(image_data, image_info);
+        enum texture_format texture_format =
+            is_srgb_encoding ? TEXTURE_FORMAT_SRGB8_A8 : TEXTURE_FORMAT_RGBA8;
+        texture = create_texture(texture_format, width, height);
+        if (texture != NULL) {
+            set_texture_pixels(texture, image_data);
+        }
+    }
 
-//     tga_free_data(image_data);
-//     tga_free_info(image_info);
-//     return texture;
-// }
+    tga_free_data(image_data);
+    tga_free_info(image_info);
+    return texture;
+}
 
-static void save_color_texture(struct texture *texture) {
+static void save_rendering_result(struct texture *texture) {
     uint32_t texture_width = get_texture_width(texture);
     uint32_t texture_height = get_texture_height(texture);
     const uint8_t *texture_data = get_texture_pixels(texture);
@@ -92,8 +118,9 @@ static void save_color_texture(struct texture *texture) {
             image_pixel[0] = texture_pixel[0];
             image_pixel[1] = texture_pixel[1];
             image_pixel[2] = texture_pixel[2];
-            // Convert all pixels to little endian.
-            endian_inversion(image_pixel, 3);
+            // Convert the pixel components to the desired arrangement order of
+            // the TGA image.
+            pixel_endian_inversion(image_pixel);
         }
     }
     // This program uses OpenGL style coordinate system, the origin of screen
@@ -120,10 +147,10 @@ static void set_shader_uniform(struct standard_uniform *uniform) {
     uniform->local2world_normal = uniform->local2world_direction;
     uniform->camera_position = CAMERA_POSITION;
     uniform->light_direction = vector3_normalize(LIGHT_DIRECTION);
-    uniform->light_intensity = VECTOR3_ONE;
-    uniform->base_color = (vector3){{1.0f, 0.0f, 0.0f}};
-    uniform->metallic = 0.0f;
-    uniform->roughness = 0.2f;
+    uniform->light_intensity = (vector3){{2.0f, 2.0f, 2.0f}};
+    uniform->base_color = VECTOR3_ONE;
+    uniform->metallic = 1.0f;
+    uniform->roughness = 1.0f;
     uniform->reflectance = 0.5f;  // Common dielectric surfaces F0.
 }
 
@@ -165,7 +192,7 @@ static void draw_model(struct mesh *mesh, struct texture *base_color_map,
         draw_triangle(framebuffer, &uniform, attribute_ptrs);
     }
 
-    save_color_texture(color_texture);
+    save_rendering_result(color_texture);
 
     // Release framebuffer.
     destroy_texture(color_texture);
@@ -174,7 +201,11 @@ static void draw_model(struct mesh *mesh, struct texture *base_color_map,
 }
 
 int main(void) {
-    const char *model_path = "assets/sphere/sphere.obj";
+    const char *model_path = "assets/suzanne/suzanne.obj";
+    const char *base_color_map_path = "assets/suzanne/base_color.tga";
+    const char *normal_map_path = "assets/suzanne/normal.tga";
+    const char *metallic_map_path = "assets/suzanne/metallic.tga";
+    const char *roughness_map_path = "assets/suzanne/roughness.tga";
 
     // Load model data.
     struct mesh *mesh;
@@ -183,13 +214,13 @@ int main(void) {
         printf("Cannot load .obj file.\n");
         return 0;
     }
-    struct texture *base_color_map = create_texture(TEXTURE_FORMAT_RGBA8, 1, 1);
-    struct texture *normal_map = create_texture(TEXTURE_FORMAT_RGBA8, 1, 1);
-    struct texture *metallic_map = create_texture(TEXTURE_FORMAT_R8, 1, 1);
-    struct texture *roughness_map = create_texture(TEXTURE_FORMAT_R8, 1, 1);
+    struct texture *base_color_map = load_texture(base_color_map_path, true);
+    struct texture *normal_map = load_texture(normal_map_path, false);
+    struct texture *metallic_map = load_texture(metallic_map_path, false);
+    struct texture *roughness_map = load_texture(roughness_map_path, false);
     if (base_color_map == NULL || normal_map == NULL || metallic_map == NULL ||
         roughness_map == NULL) {
-        printf("Cannot create texture.\n");
+        printf("Cannot load texture files.\n");
         destroy_mesh(mesh);
         destroy_texture(base_color_map);
         destroy_texture(normal_map);
@@ -197,15 +228,6 @@ int main(void) {
         destroy_texture(roughness_map);
         return 0;
     }
-    // Set textures to their respective defaults, equivalent to not using
-    // textures.
-    uint8_t base_color_data[3] = {255, 255, 255};
-    set_texture_pixels(base_color_map, base_color_data);
-    uint8_t normal_data[3] = {128, 128, 255};
-    set_texture_pixels(normal_map, normal_data);
-    uint8_t white = 255;
-    set_texture_pixels(metallic_map, &white);
-    set_texture_pixels(roughness_map, &white);
 
     draw_model(mesh, base_color_map, normal_map, metallic_map, roughness_map);
 
