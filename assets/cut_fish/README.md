a9233d73f6b9f5645e0551ed4278d4883fa1d1ec
cadenji <cadenji@qq.com>
2022-06-06 23:18:51 +0800

    Add Cut Fash model to demonstrate rendering result
    The model "Cut Fish" by Suushimi, Licensed under
    the CC BY-NC.

---
+++ assets/cut_fish/README.md
@@ -0,0 +1,14 @@
+# Cut Fish
+
+## Description
+
+Upstream: https://sketchfab.com/3d-models/cut-fish-c9bcb782e5ef4d37bd8671f1f82a0fbc
+
+This model has been imported and re-exported from Blender to adjust the position and convert all faces to triangles. The textures has been converted to TGA format.
+
+## License Information
+
+[![CC BY-NC](https://i.creativecommons.org/l/by-nc/4.0/88x31.png)](https://creativecommons.org/licenses/by-nc/4.0/)
+
+[Suushimi](https://sketchfab.com/Suushimi) is the author of these assets for the purpose of copyright. All rights reserved.
+Licensed under the [Creative Commons Attribution-NonCommercial 4.0 International License](https://creativecommons.org/licenses/by-nc/4.0/).
--- foolrenderer/main.c
+++ foolrenderer/main.c
@@ -20,15 +20,20 @@
 #define IMAGE_WIDTH 1024
 #define IMAGE_HEIGHT 1024
 
-const vector3 LIGHT_DIRECTION = {{1.0f, 1.0f, 0.5f}};
-const vector3 CAMERA_POSITION = {{0.0f, 0.0f, 2.5f}};
+static const vector3 LIGHT_DIRECTION = {{1.0f, 4.0f, -1.0f}};
+static const vector3 CAMERA_POSITION = {{-2.0f, 4.5f, 2.0f}};
+
+static struct texture *base_color_map;
+static struct texture *normal_map;
+static struct texture *metallic_map;
+static struct texture *roughness_map;
 
 static void set_shader_uniform(struct standard_uniform *uniform) {
     uniform->local2world = MATRIX4X4_IDENTITY;
-    matrix4x4 world2view = matrix4x4_look_at(CAMERA_POSITION, VECTOR3_ZERO,
-                                             (vector3){{0.0f, 1.0f, 0.0f}});
-    matrix4x4 view2clip = matrix4x4_perspective(
-        PI / 4.0f, IMAGE_WIDTH / IMAGE_HEIGHT, 0.1f, 5.0f);
+    matrix4x4 world2view =
+        matrix4x4_look_at(CAMERA_POSITION, (vector3){{0.0f, 0.4f, 0.0f}},
+                          (vector3){{0.0f, 1.0f, 0.0f}});
+    matrix4x4 view2clip = matrix4x4_orthographic(2.0f, 2.0f, 0.1f, 10.0f);
     uniform->world2clip = matrix4x4_multiply(view2clip, world2view);
     uniform->local2world_direction = matrix4x4_to_3x3(uniform->local2world);
     // There is no non-uniform scaling so the normal transformation matrix is
@@ -36,17 +41,19 @@ static void set_shader_uniform(struct standard_uniform *uniform) {
     uniform->local2world_normal = uniform->local2world_direction;
     uniform->camera_position = CAMERA_POSITION;
     uniform->light_direction = vector3_normalize(LIGHT_DIRECTION);
-    uniform->illuminance = (vector3){{2.0f, 2.0f, 2.0f}};
-    uniform->ambient_luminance = (vector3){{0.02f, 0.02f, 0.02f}};
+    uniform->illuminance = (vector3){{4.0f, 4.0f, 4.0f}};
+    uniform->ambient_luminance = (vector3){{1.0f, 0.5f, 0.8f}};
+    uniform->normal_map = normal_map;
     uniform->base_color = VECTOR3_ONE;
+    uniform->base_color_map = base_color_map;
     uniform->metallic = 1.0f;
+    uniform->metallic_map = metallic_map;
     uniform->roughness = 1.0f;
+    uniform->roughness_map = roughness_map;
     uniform->reflectance = 0.5f;  // Common dielectric surfaces F0.
 }
 
-static void draw_model(struct mesh *mesh, struct texture *base_color_map,
-                       struct texture *normal_map, struct texture *metallic_map,
-                       struct texture *roughness_map) {
+static void draw_model(struct mesh *mesh) {
     struct framebuffer *framebuffer = create_framebuffer();
     struct texture *color_texture =
         create_texture(TEXTURE_FORMAT_SRGB8_A8, IMAGE_WIDTH, IMAGE_HEIGHT);
@@ -63,10 +70,6 @@ static void draw_model(struct mesh *mesh, struct texture *base_color_map,
 
     struct standard_uniform uniform;
     set_shader_uniform(&uniform);
-    uniform.base_color_map = base_color_map;
-    uniform.normal_map = normal_map;
-    uniform.metallic_map = metallic_map;
-    uniform.roughness_map = roughness_map;
 
     uint32_t triangle_count = mesh->triangle_count;
     for (size_t t = 0; t < triangle_count; t++) {
@@ -91,11 +94,11 @@ static void draw_model(struct mesh *mesh, struct texture *base_color_map,
 }
 
 int main(void) {
-    const char *model_path = "assets/sphere/sphere.obj";
-    const char *base_color_map_path = "assets/rusted_iron/base_color.tga";
-    const char *normal_map_path = "assets/rusted_iron/normal.tga";
-    const char *metallic_map_path = "assets/rusted_iron/metallic.tga";
-    const char *roughness_map_path = "assets/rusted_iron/roughness.tga";
+    const char *model_path = "assets/cut_fish/cut_fish.obj";
+    const char *base_color_map_path = "assets/cut_fish/base_color.tga";
+    const char *normal_map_path = "assets/cut_fish/normal.tga";
+    const char *metallic_map_path = "assets/cut_fish/metallic.tga";
+    const char *roughness_map_path = "assets/cut_fish/roughness.tga";
 
     // Load model data.
     struct mesh *mesh;
@@ -104,10 +107,10 @@ int main(void) {
         printf("Cannot load .obj file.\n");
         return 0;
     }
-    struct texture *base_color_map = load_image(base_color_map_path, true);
-    struct texture *normal_map = load_image(normal_map_path, false);
-    struct texture *metallic_map = load_image(metallic_map_path, false);
-    struct texture *roughness_map = load_image(roughness_map_path, false);
+    base_color_map = load_image(base_color_map_path, true);
+    normal_map = load_image(normal_map_path, false);
+    metallic_map = load_image(metallic_map_path, false);
+    roughness_map = load_image(roughness_map_path, false);
     if (base_color_map == NULL || normal_map == NULL || metallic_map == NULL ||
         roughness_map == NULL) {
         printf("Cannot load texture files.\n");
@@ -119,7 +122,7 @@ int main(void) {
         return 0;
     }
 
-    draw_model(mesh, base_color_map, normal_map, metallic_map, roughness_map);
+    draw_model(mesh);
 
     destroy_mesh(mesh);
     destroy_texture(base_color_map);
