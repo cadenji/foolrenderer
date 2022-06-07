a0cad3902ef05c765aced57818b0a20dd7a0e236
cadenji <cadenji@qq.com>
2022-06-07 23:59:31 +0800

    Modify the code related to the rendering model
    Now framebuffers and buffers are declared as global
    variables for easy access between functions.
    
    light_direction and camera_position are now modifiable
    variables.

--- foolrenderer/main.c
+++ foolrenderer/main.c
@@ -20,57 +20,68 @@
 #define IMAGE_WIDTH 1024
 #define IMAGE_HEIGHT 1024
 
-static const vector3 LIGHT_DIRECTION = {{1.0f, 4.0f, -1.0f}};
-static const vector3 CAMERA_POSITION = {{-2.0f, 4.5f, 2.0f}};
+struct model {
+    struct mesh *mesh;
+    struct texture *base_color_map;
+    struct texture *normal_map;
+    struct texture *metallic_map;
+    struct texture *roughness_map;
+};
 
-static struct texture *base_color_map;
-static struct texture *normal_map;
-static struct texture *metallic_map;
-static struct texture *roughness_map;
+static vector3 light_direction = (vector3){{1.0f, 4.0f, -1.0f}};
+static vector3 camera_position = (vector3){{-2.0f, 4.5f, 2.0f}};
 
-static void set_shader_uniform(struct standard_uniform *uniform) {
-    uniform->local2world = MATRIX4X4_IDENTITY;
-    matrix4x4 world2view =
-        matrix4x4_look_at(CAMERA_POSITION, (vector3){{0.0f, 0.4f, 0.0f}},
-                          (vector3){{0.0f, 1.0f, 0.0f}});
-    matrix4x4 view2clip = matrix4x4_orthographic(2.0f, 2.0f, 0.1f, 10.0f);
-    uniform->world2clip = matrix4x4_multiply(view2clip, world2view);
-    uniform->local2world_direction = matrix4x4_to_3x3(uniform->local2world);
-    // There is no non-uniform scaling so the normal transformation matrix is
-    // the direction transformation matrix.
-    uniform->local2world_normal = uniform->local2world_direction;
-    uniform->camera_position = CAMERA_POSITION;
-    uniform->light_direction = vector3_normalize(LIGHT_DIRECTION);
-    uniform->illuminance = (vector3){{4.0f, 4.0f, 4.0f}};
-    uniform->ambient_luminance = (vector3){{1.0f, 0.5f, 0.8f}};
-    uniform->normal_map = normal_map;
-    uniform->base_color = VECTOR3_ONE;
-    uniform->base_color_map = base_color_map;
-    uniform->metallic = 1.0f;
-    uniform->metallic_map = metallic_map;
-    uniform->roughness = 1.0f;
-    uniform->roughness_map = roughness_map;
-    uniform->reflectance = 0.5f;  // Common dielectric surfaces F0.
+static struct framebuffer *framebuffer;
+struct texture *color_buffer;
+struct texture *depth_buffer;
+
+static void initialize_rendering(void) {
+    framebuffer = create_framebuffer();
+    color_buffer =
+        create_texture(TEXTURE_FORMAT_SRGB8_A8, IMAGE_WIDTH, IMAGE_HEIGHT);
+    depth_buffer =
+        create_texture(TEXTURE_FORMAT_DEPTH_FLOAT, IMAGE_WIDTH, IMAGE_HEIGHT);
+    attach_texture_to_framebuffer(framebuffer, COLOR_ATTACHMENT, color_buffer);
+    attach_texture_to_framebuffer(framebuffer, DEPTH_ATTACHMENT, depth_buffer);
 }
 
-static void draw_model(struct mesh *mesh) {
-    struct framebuffer *framebuffer = create_framebuffer();
-    struct texture *color_texture =
-        create_texture(TEXTURE_FORMAT_SRGB8_A8, IMAGE_WIDTH, IMAGE_HEIGHT);
-    struct texture *depth_texture =
-        create_texture(TEXTURE_FORMAT_DEPTH_FLOAT, IMAGE_WIDTH, IMAGE_HEIGHT);
-    attach_texture_to_framebuffer(framebuffer, COLOR_ATTACHMENT, color_texture);
-    attach_texture_to_framebuffer(framebuffer, DEPTH_ATTACHMENT, depth_texture);
+static void end_rendering(void) {
+    destroy_texture(color_buffer);
+    destroy_texture(depth_buffer);
+    destroy_framebuffer(framebuffer);
+}
 
-    // Draw the model.
+static void render_model(const struct model *model) {
     set_viewport(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
     set_vertex_shader(standard_vertex_shader);
     set_fragment_shader(standard_fragment_shader);
     clear_framebuffer(framebuffer);
 
     struct standard_uniform uniform;
-    set_shader_uniform(&uniform);
+    uniform.local2world = MATRIX4X4_IDENTITY;
+    matrix4x4 world2view =
+        matrix4x4_look_at(camera_position, (vector3){{0.0f, 0.4f, 0.0f}},
+                          (vector3){{0.0f, 1.0f, 0.0f}});
+    matrix4x4 view2clip = matrix4x4_orthographic(2.0f, 2.0f, 0.1f, 10.0f);
+    uniform.world2clip = matrix4x4_multiply(view2clip, world2view);
+    uniform.local2world_direction = matrix4x4_to_3x3(uniform.local2world);
+    // There is no non-uniform scaling so the normal transformation matrix is
+    // the direction transformation matrix.
+    uniform.local2world_normal = uniform.local2world_direction;
+    uniform.camera_position = camera_position;
+    uniform.light_direction = vector3_normalize(light_direction);
+    uniform.illuminance = (vector3){{4.0f, 4.0f, 4.0f}};
+    uniform.ambient_luminance = (vector3){{1.0f, 0.5f, 0.8f}};
+    uniform.normal_map = model->normal_map;
+    uniform.base_color = VECTOR3_ONE;
+    uniform.base_color_map = model->base_color_map;
+    uniform.metallic = 1.0f;
+    uniform.metallic_map = model->metallic_map;
+    uniform.roughness = 1.0f;
+    uniform.roughness_map = model->roughness_map;
+    uniform.reflectance = 0.5f;  // Common dielectric surfaces F0.
 
+    const struct mesh *mesh = model->mesh;
     uint32_t triangle_count = mesh->triangle_count;
     for (size_t t = 0; t < triangle_count; t++) {
         struct standard_vertex_attribute attributes[3];
@@ -84,13 +95,7 @@ static void draw_model(struct mesh *mesh) {
         }
         draw_triangle(framebuffer, &uniform, attribute_ptrs);
     }
-
-    save_image(color_texture, "output.tga");
-
-    // Release framebuffer.
-    destroy_texture(color_texture);
-    destroy_texture(depth_texture);
-    destroy_framebuffer(framebuffer);
+    save_image(color_buffer, "output.tga");
 }
 
 int main(void) {
@@ -100,34 +105,35 @@ int main(void) {
     const char *metallic_map_path = "assets/cut_fish/metallic.tga";
     const char *roughness_map_path = "assets/cut_fish/roughness.tga";
 
-    // Load model data.
-    struct mesh *mesh;
-    mesh = load_mesh(model_path);
-    if (mesh == NULL) {
+    struct model model;
+    model.mesh = load_mesh(model_path);
+    if (model.mesh == NULL) {
         printf("Cannot load .obj file.\n");
         return 0;
     }
-    base_color_map = load_image(base_color_map_path, true);
-    normal_map = load_image(normal_map_path, false);
-    metallic_map = load_image(metallic_map_path, false);
-    roughness_map = load_image(roughness_map_path, false);
-    if (base_color_map == NULL || normal_map == NULL || metallic_map == NULL ||
-        roughness_map == NULL) {
+    model.base_color_map = load_image(base_color_map_path, true);
+    model.normal_map = load_image(normal_map_path, false);
+    model.metallic_map = load_image(metallic_map_path, false);
+    model.roughness_map = load_image(roughness_map_path, false);
+    if (model.base_color_map == NULL || model.normal_map == NULL ||
+        model.metallic_map == NULL || model.roughness_map == NULL) {
         printf("Cannot load texture files.\n");
-        destroy_mesh(mesh);
-        destroy_texture(base_color_map);
-        destroy_texture(normal_map);
-        destroy_texture(metallic_map);
-        destroy_texture(roughness_map);
+        destroy_mesh(model.mesh);
+        destroy_texture(model.base_color_map);
+        destroy_texture(model.normal_map);
+        destroy_texture(model.metallic_map);
+        destroy_texture(model.roughness_map);
         return 0;
     }
 
-    draw_model(mesh);
+    initialize_rendering();
+    render_model(&model);
+    end_rendering();
 
-    destroy_mesh(mesh);
-    destroy_texture(base_color_map);
-    destroy_texture(normal_map);
-    destroy_texture(metallic_map);
-    destroy_texture(roughness_map);
+    destroy_mesh(model.mesh);
+    destroy_texture(model.base_color_map);
+    destroy_texture(model.normal_map);
+    destroy_texture(model.metallic_map);
+    destroy_texture(model.roughness_map);
     return 0;
 }
